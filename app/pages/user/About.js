7ac86ae8fa2df134f4c0e3a63c93d1dfb7edd232
赵坤 <zk8087@gmail.com>
2019-08-14 11:48:58 +0800

    8-14

--- app/pages/user/About.js
+++ app/pages/user/About.js
@@ -17,11 +17,11 @@ export default class About extends BaseComponent{
                         height:66, alignItems:'center'}} resizeMode='cover'/>
                 </ImageBackground>
                 <View style={{alignItems:'center',justifyContent:'flex-start',flexDirection:'row',height:50,marginLeft:10}}>
-                    <Text>宜居</Text>
+                    <Text>幸福宜居</Text>
                 </View>
                 <View style={{height: 0.5, backgroundColor: CommonStyle.lineColor, width: '100%',marginRight: 10,marginLeft: 10}}/>
                 <View style={{alignItems:'center',justifyContent:'flex-start',flexDirection:'row',margin:10}}>
-                    <Text style={{fontSize:14,color:'#666'}}>这里是关于</Text>
+                    <Text style={{fontSize:14,color:'#666'}}>“幸福宜居”APP是由亳州市宜居物业管理有限公司拥有自主产权、并由安徽启凡科技研发，为宜居物业业主量身定制的融合了基础物业服务、园区生活服务、邻里社交服务等的一站式生活服务平台，致力于为广大宜居业主打造更安全、更便捷、更和睦的幸福园区。</Text>
                 </View>
             </View>
         );
